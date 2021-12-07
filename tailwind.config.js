79451c4050e3faa596824297a83bace772f3eceb
phuc.nguyen <phuc.nguyen@viezone.vn>
2021-12-07 13:22:14 +0700

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,73 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
 const Colors = {
   "s-black": "#2A2C38",
+  "s-gray": "#87929d",
+  primary: "#007af0",
+  "neutral-tile-active": "#fcfcfc",
+  "neutral-body": "#C8CCD0",
 };
-
+console.log(process.env.NODE_ENV);
 module.exports = {
+  mode: "jit",
   purge: {
-    enabled: true,
+    // enabled: process.env.NODE_ENV === "production",
+    enabled: false,
     content: ["src/*.html"],
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    backgroundColor: (theme) => ({
-      ...theme("colors"),
-      ...Colors,
+    fontSize: (theme) => ({
+      ...defaultTheme.fontSize,
+      h1: [
+        "80px",
+        {
+          lineHeight: "88px",
+          letterSpacing: "-0.03em",
+        },
+      ],
+      h2: [
+        "48px",
+        {
+          lineHeight: "56px",
+          letterSpacing: "-0.03em",
+        },
+      ],
+      caption1: [
+        "20px",
+        {
+          lineHeight: "32px",
+          letterSpacing: "-0.02em",
+        },
+      ],
+      caption2: [
+        "20px",
+        {
+          lineHeight: "32px",
+          letterSpacing: "-0.02em",
+        },
+      ],
     }),
-    extend: {},
+    extend: {
+      fontFamily: {
+        "primary-medium": ["Inter-Medium", "sans-serif"],
+        "primary-regular": ["Inter-Regular", "sans-serif"],
+        "primary-extralight": ["Inter-ExtraLight", "sans-serif"],
+        "secondary-medium": ["Manrope-Medium", "sans-serif"],
+        "secondary-regular": ["Manrope-Regular", "sans-serif"],
+      },
+      colors: {
+        ...Colors,
+      },
+      borderColor: {
+        ...Colors,
+      },
+      textColor: {
+        ...Colors,
+      },
+      backgroundColor: {
+        ...Colors,
+      },
+    },
   },
   variants: {
     extend: {},
