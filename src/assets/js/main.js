756aaa29bc4005831635fc60bdb380d5eb4abf7a
hoangphuc17 <phuc.nh94@gmail.com>
2021-12-09 01:20:19 +0700

    wip

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,24 +1,29 @@
 window.addEventListener("DOMContentLoaded", (event) => {
-  setTimeout(function () {
-    var sec1Ele = document.getElementById("sec1");
-    if (!sec1Ele.classList.contains("active")) {
-      sec1Ele.classList.add("active");
-    }
-  }, 2000);
+  // setTimeout(function () {
+  //   var sec1Ele = document.getElementById("sec1");
+  //   if (!sec1Ele.classList.contains("active")) {
+  //     sec1Ele.classList.add("active");
+  //   }
+  // }, 2000);
 
   // init controller
   var controller = new ScrollMagic.Controller({});
 
   // build scenes
-  // new ScrollMagic.Scene({
-  //   triggerElement: "#sec1",
-
-  //  })
-  //   .setClassToggle("#img-app-preview", "active") // add class toggle
-  //   .addIndicators() // add indicators (requires plugin)
-  //   .addTo(controller);
-  new ScrollMagic.Scene({ triggerElement: "#sec2" })
+  new ScrollMagic.Scene({
+    triggerElement: "#sec1",
+    triggerHook: "onLeave",
+    duration: 500,
+  })
     .setClassToggle("#sec1", "active") // add class toggle
+    .addIndicators() // add indicators (requires plugin)
+    .addTo(controller);
+  new ScrollMagic.Scene({ triggerElement: "#sec2", duration: 1000 })
+    .setClassToggle("#sec2", "active") // add class toggle
+    .addIndicators() // add indicators (requires plugin)
+    .addTo(controller);
+  new ScrollMagic.Scene({ triggerElement: "#sec3", duration: 700 })
+    .setClassToggle("#sec3", "active") // add class toggle
     .addIndicators() // add indicators (requires plugin)
     .addTo(controller);
   // new ScrollMagic.Scene({ triggerElement: "#sec2" })
