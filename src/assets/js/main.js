46875087af6251a824594560052e86957a767a7c
phuc.nguyen <phuc.nguyen@viezone.vn>
2021-12-09 12:41:50 +0700

    animation

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,38 +1,63 @@
-window.addEventListener("DOMContentLoaded", (event) => {
-  // setTimeout(function () {
-  //   var sec1Ele = document.getElementById("sec1");
-  //   if (!sec1Ele.classList.contains("active")) {
-  //     sec1Ele.classList.add("active");
-  //   }
-  // }, 2000);
+function getDeviceWidth(device, mobileWidth, desktopWidth) {
+  return device === "desktop" ? desktopWidth : mobileWidth;
+}
 
-  // init controller
+window.addEventListener("DOMContentLoaded", (event) => {
+  var deviceWidth = window.innerWidth > 0 ? window.innerWidth : screen.width;
+  var device = deviceWidth < 1280 ? "mobile" : "desktop";
   var controller = new ScrollMagic.Controller({});
 
-  // build scenes
   new ScrollMagic.Scene({
     triggerElement: "#sec1",
     triggerHook: "onLeave",
-    duration: 500,
+    duration: getDeviceWidth(device, 400, 600),
+    offset: getDeviceWidth(device, -110, -80),
   })
     .setClassToggle("#sec1", "active") // add class toggle
-    .addIndicators() // add indicators (requires plugin)
+    // .addIndicators() // add indicators (requires plugin)
     .addTo(controller);
   new ScrollMagic.Scene({ triggerElement: "#sec2", duration: 1000 })
     .setClassToggle("#sec2", "active") // add class toggle
-    .addIndicators() // add indicators (requires plugin)
+    // .addIndicators() // add indicators (requires plugin)
     .addTo(controller);
-  new ScrollMagic.Scene({ triggerElement: "#sec3", duration: 700 })
+  new ScrollMagic.Scene({ triggerElement: "#sec3", duration: 1000 })
     .setClassToggle("#sec3", "active") // add class toggle
-    .addIndicators() // add indicators (requires plugin)
+    // .addIndicators() // add indicators (requires plugin)
     .addTo(controller);
-  // new ScrollMagic.Scene({ triggerElement: "#sec2" })
-  //   .setClassToggle("#high2", "active") // add class toggle
-  //   .addTo(controller);
-  // new ScrollMagic.Scene({ triggerElement: "#sec3" })
-  //   .setClassToggle("#high3", "active") // add class toggle
-  //   .addTo(controller);
-  // new ScrollMagic.Scene({ triggerElement: "#sec4" })
-  //   .setClassToggle("#high4", "active") // add class toggle
-  //   .addTo(controller);
+  new ScrollMagic.Scene({
+    triggerElement: "#sec4",
+    duration: getDeviceWidth(device, 800, 1000),
+  })
+    .setClassToggle("#sec4", "active") // add class toggle
+    // .addIndicators() // add indicators (requires plugin)
+    .addTo(controller);
+  new ScrollMagic.Scene({
+    triggerElement: "#sec5",
+    duration: 1000,
+    offset: 200,
+  })
+    .setClassToggle("#sec5", "active") // add class toggle
+    // .addIndicators() // add indicators (requires plugin)
+    .addTo(controller);
+  new ScrollMagic.Scene({
+    triggerElement: "#sec6",
+    duration: getDeviceWidth(device, 1700, 1200),
+  })
+    .setClassToggle("#sec6", "active") // add class toggle
+    // .addIndicators() // add indicators (requires plugin)
+    .addTo(controller);
+  new ScrollMagic.Scene({
+    triggerElement: "#sec7",
+    duration: getDeviceWidth(device, 900, 1000),
+  })
+    .setClassToggle("#sec7", "active") // add class toggle
+    // .addIndicators() // add indicators (requires plugin)
+    .addTo(controller);
+  new ScrollMagic.Scene({
+    triggerElement: "#sec8",
+    duration: getDeviceWidth(device, 1200, 500),
+  })
+    .setClassToggle("#sec8", "active") // add class toggle
+    // .addIndicators() // add indicators (requires plugin)
+    .addTo(controller);
 });
